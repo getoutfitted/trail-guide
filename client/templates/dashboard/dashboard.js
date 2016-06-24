3e2ea55c7b91366a7a6ce69bc13397c511d92791
Paul Grever <paulgrever@gmail.com>
2016-06-24 15:13:07 -0600

    refactored to make filters better and more meteor

--- client/templates/dashboard/dashboard.js
+++ client/templates/dashboard/dashboard.js
@@ -1,47 +1,46 @@
-const defaultDisplayFields = [
-  'orderNumber',
-  'customer',
-  'contact',
-  'total',
-  'createdAt',
-  'transit',
-  'shipping',
-  'rentalStart',
-  'rentalEnd'
-];
+const DefaultFields = {
+  'orderNumber': '#',
+  'billing.address.fullName': 'Customer',
+  'email': 'Email',
+  'billing.address.phone': 'Phone',
+  'billing.invoice.total': 'Total',
+  'createdAt': 'Created',
+  'advancedFulfillment.transitTime': 'Transit',
+  'advancedFulfillment.shipmentDate': 'Shipping',
+  'startTime': 'Rental Start',
+  'endTime': 'Rental End',
+  'advancedFulfillment.returnDate': 'Returning',
+  'rentalDays': 'Length',
+  'shipping.address.region': 'Ship To',
+  'billing.address.region': 'Bill To',
+  'advancedFulfillment.workflow.status': 'Status',
+  'advancedFulfillment.localDelivery': 'Local'
+};
 
-// const HumanReadable = {
-//   'orderNumber': 'Order Number',
-//   'customer': 'Customer',
-//   'contact': 'Contact',
-//   'billing.invoice.total': 'Total'
-// };
-
-// const Sortable = {
-//   'orderNumber': true,
-//   'customer': false,
-//   'contact': false,
-//   'billing.invoice.total': true
-// };
 
 Template.trailGuideDashboard.onCreated(function () {
   Session.setDefault('sortField', 'orderNumber');
   Session.setDefault('sortOrder', -1);
   Session.setDefault('limit', 20);
-  Session.setDefault('customerEnabled', true);
-  let displayFields = defaultDisplayFields;
+  let displayFields = Object.keys(DefaultFields);
   let defaultDisplay = {};
   _.each(displayFields, function (df) {
-    defaultDisplay[df] = true;
+    const nonDefault = ['email', 'billing.address.phone', 'advancedFulfillment.localDelivery'];
+    if (_.contains(nonDefault, df)) {
+      defaultDisplay[df] = false;
+    } else {
+      defaultDisplay[df] = true;
+    }
   });
   Session.setDefault('enabledFields', defaultDisplay);
   this.autorun(() => {
     let field = Session.get('sortField');
     let order = Session.get('sortOrder');
     let limit = Session.get('limit') || 20;
+    let activeFields = Session.get('enabledFields');
     let sort = {};
     sort[field] = order;
-    this.subscribe('trailGuideAllOrders', limit, sort);
+    this.subscribe('trailGuideAllOrders', limit, sort, activeFields);
   });
 });
 
@@ -55,49 +54,17 @@ Template.trailGuideDashboard.helpers({
       sort: sortOrder
     });
   },
-  upOrDown: function (field) {
-    if (Session.get('sortField') === field && Session.get('sortOrder') === -1) {
-      return `<i data-field=${field} class="fa fa-caret-up change" aria-hidden="true"></i>`;
-    }
-    return `<i data-field="${field}" class="fa fa-caret-down change" aria-hidden="true"></i>`;
-  },
-  clearDate: function (date) {
-    return moment(date).format('M/D/YY');
-  },
-  region: function (type) {
-    return this[type][0].address.region;
-  },
-  customer: function () {
-    return this.billing[0].address.fullName;
-  },
-  contactInfo: function () {
-    let email = this.email;
-    let phone = this.billing[0].address.phone;
-    return `<p>${email}</p><p>${phone}</p>`;
-  },
-  humanizeStatus: function () {
-    return AdvancedFulfillment.humanOrderStatuses[this.advancedFulfillment.workflow.status];
-  },
-  total: function () {
-    return this.billing[0].invoice.total;
-  },
-  sortingBy: function (field) {
-    if (Session.get('sortField') === field) {
-      return 'active';
-    }
-    return '';
-  },
   currentLimit: function () {
     return Session.get('limit');
-  },
-  customerEnabled: function () {
-    return Session.get('customerEnabled');
   },
   fields: function (field) {
     return Session.get('enabledFields')[field];
   },
   displayFields: function () {
-    return defaultDisplayFields;
+    return Object.keys(DefaultFields);
+  },
+  humanReadableField: function (field) {
+    return DefaultFields[field];
   }
 });
 
@@ -124,5 +91,80 @@ Template.trailGuideDashboard.events({
     let displaySettings = Session.get('enabledFields');
     displaySettings[field] = !displaySettings[field];
     Session.set('enabledFields', displaySettings);
+  }
+});
+
+Template.columnGroupHeader.helpers({
+  enabled: function () {
+    const str = this.valueOf();
+    return Session.get('enabledFields')[str];
+  },
+  active: function () {
+    const str = this.valueOf();
+    if (Session.get('sortField') === str) {
+      return 'active';
+    }
+    return '';
+  }
+});
+
+Template.columnMainHeaders.helpers({
+  enabled: function () {
+    const str = this.valueOf();
+    return Session.get('enabledFields')[str];
+  },
+  label: function () {
+    const str = this.valueOf();
+    return DefaultFields[str];
+  },
+  upOrDown: function () {
+    const field = this.valueOf();
+    if (Session.get('sortField') === field && Session.get('sortOrder') === -1) {
+      return 'upArrow';
+    }
+    return 'downArrow';
+  }
+});
+
+Template.columnMainRow.helpers({
+  enabled: function () {
+    const str = this.valueOf();
+    return Session.get('enabledFields')[str];
+  },
+  orderInfo: function () {
+    const field = this.valueOf();
+    switch (field) {
+    case 'billing.address.fullName':
+      return Template.parentData().billing[0].address.fullName;
+    case 'billing.address.phone':
+      return Template.parentData().billing[0].address.phone;
+    case 'advancedFulfillment.localDelivery':
+    case 'advancedFulfillment.transitTime':
+      const afField = field.split('.')[1];
+      return Template.parentData().advancedFulfillment[afField];
+    // Using Fall through technique which is || in Switch
+    case 'createdAt':
+    case 'startTime':
+    case 'endTime':
+      const date = Template.parentData()[this.valueOf()];
+      return moment(date).format('M/D/YY');
+    case 'advancedFulfillment.shipmentDate':
+      const shipDate = Template.parentData().advancedFulfillment.shipmentDate;
+      return moment(shipDate).format('M/D/YY');
+    case 'advancedFulfillment.returnDate':
+      const returnDate = Template.parentData().advancedFulfillment.returnDate;
+      return moment(returnDate).format('M/D/YY');
+    case 'shipping.address.region':
+    case 'billing.address.region':
+      const billingOrShipping = field.split('.')[0];
+      return Template.parentData()[billingOrShipping][0].address.region;
+    case 'billing.invoice.total':
+      return `$${Template.parentData().billing[0].invoice.total}`;
+    case 'advancedFulfillment.workflow.status':
+      const status = Template.parentData().advancedFulfillment.workflow.status;
+      return AdvancedFulfillment.humanOrderStatuses[status];
+    default:
+      return Template.parentData()[this.valueOf()];
+    }
   }
 });
--- server/publications/trailGuide.js
+++ server/publications/trailGuide.js
@@ -1,13 +1,21 @@
-Meteor.publish('trailGuideAllOrders', function (limit, sort) {
+Meteor.publish('trailGuideAllOrders', function (limit, sort, fields) {
   check(limit, Number);
   check(sort, Object);
+  check(fields, Object);
+  let returnFields = {};
+  _.each(fields, function (value, index) {
+    if (value) {
+      returnFields[index] = 1;
+    }
+  });
   const shopId = ReactionCore.getShopId();
   if (Roles.userIsInRole(this.userId, 'trail-guide', ReactionCore.getShopId())) {
     return ReactionCore.Collections.Orders.find({
       shopId: shopId
     }, {
       limit: limit,
-      sort: sort
+      sort: sort,
+      fields: returnFields
     });
   }
 });
